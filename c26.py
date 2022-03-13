dad67c6aabd0aa1b7aa126853c27fdfa09d0076c
commandblockguy <bluestemjc@gmail.com>
2022-03-12 22:06:16 -0600

    Fix challenge 26

--- c26.py
+++ c26.py
@@ -4,7 +4,7 @@ key = random_bytes(16)
 
 def encrypt(data):
 	nonce = random.randrange(0, 2**32)
-	plaintext = 'comment1=cooking%20MCs;userdata=' + data.replace(';', '%3B').replace('=', '%3D') + ';comment2=%20like%20a%20pound%20of%20bacon'
+	plaintext = b'comment1=cooking%20MCs;userdata=' + data.replace(b';', b'%3B').replace(b'=', b'%3D') + b';comment2=%20like%20a%20pound%20of%20bacon'
 	return encrypt_aes_ctr(plaintext, key, nonce), nonce
 
 def decrypt(data, nonce):
@@ -12,3 +12,9 @@ def decrypt(data, nonce):
 
 def is_admin(data):
 	return b'admin=true' in data.split(b';')
+
+ct, nonce = encrypt(b'\x00' * 11)
+ct = ct[:32] + xor_bytes(b';admin=true', ct[32:43]) + ct[43:]
+pt = decrypt(ct, nonce)
+print(pt)
+print(is_admin(pt))
